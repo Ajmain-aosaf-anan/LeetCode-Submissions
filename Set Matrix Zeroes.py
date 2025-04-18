1f4411fbd108caa8df52e34807ad4d391dc92d09
Ajmain Aosaf Anan <64554433+Ajmain-aosaf-anan@users.noreply.github.com>
2025-04-18 17:38:58 +0600

    Create Set Matrix Zeroes.py

---
+++ Set Matrix Zeroes.py
@@ -0,0 +1,27 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+      rows, cols = len(matrix), len(matrix[0])
+      row_zero = False
+
+      for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        row_zero = True
+
+        # Set elements to zero based on markers
+      for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+      if matrix[0][0] == 0:
+            for i in range(rows):
+                matrix[i][0] = 0
+
+      if row_zero:
+            for j in range(cols):
+                matrix[0][j] = 0
